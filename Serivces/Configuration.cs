da3d7764f0223439528c394d713d3e1e0ab343be
Kauã Q. <89699012+kaua-alves-queiros@users.noreply.github.com>
2025-09-10 15:57:03 -0400

    criado lib setup

---
+++ Serivces/Configuration.cs
@@ -0,0 +1,12 @@
+﻿namespace UispNet.Services;
+
+public partial class UispNetService
+{
+    private HttpClient _httpClient = new HttpClient();
+    
+    public void Setup(string host, string appKey)
+    {
+        _httpClient.BaseAddress = new Uri(host);
+        _httpClient.DefaultRequestHeaders.Add("X-Auth-App-Key", appKey);
+    }
+}
